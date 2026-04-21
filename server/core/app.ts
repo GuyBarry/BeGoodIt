6e6f1d798507f44149f44733b0b03f05154c7bb1
mayanamsterdam <maayanamsterdam@gmail.com>
2026-04-21 11:49:13 +0300

    feat: add noRoute middleware for handling undefined routes

--- server/core/app.ts
+++ server/core/app.ts
@@ -1,28 +1,30 @@
-import 'dotenv/config';
-import express, { Express, Request, Response } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './src/config/swagger.config';
-import { errorHandler } from './src/middlewares/error.middleware';
-import { serverConfig } from './src/config/server.config';
-import { imagesRouter } from './src/images/images.controller';
+import "dotenv/config";
+import express, { Express, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./src/config/swagger.config";
+import { errorHandler } from "./src/middlewares/error.middleware";
+import { noRouteHandler } from "./src/middlewares/noRoute.middleware";
+import { serverConfig } from "./src/config/server.config";
+import { imagesRouter } from "./src/images/images.controller";
 
 const app: Express = express();
 const PORT = serverConfig.port;
 
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get("/", (_req: Request, res: Response) => {
+  res.json({ message: "Welcome to BeGoodIt API" });
+});
 
 app.use(imagesRouter);
 
+app.use(noRouteHandler);
 app.use(errorHandler);
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Welcome to BeGoodIt API' });
-});
-
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-  console.log(`API docs available at http://localhost:${PORT}/api-docs`);
+  console.log(`Listening on port ${PORT}`);
 });
 
 export default app;
