1843de98b173b66ecf17a47155647e45ae92212b
mayanamsterdam <maayanamsterdam@gmail.com>
2026-04-21 18:18:48 +0300

    add docs for routers

---
+++ server/core/src/controllers/docs/colorGroup.controller.docs.ts
@@ -0,0 +1,35 @@
+/**
+ * @swagger
+ * /color-groups:
+ *   get:
+ *     summary: Get all color groups
+ *     description: Returns a list of all available color groups.
+ *     tags:
+ *       - Color Groups
+ *     responses:
+ *       200:
+ *         description: A list of color groups
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   colorId:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: Warm
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Oops, something went wrong!
+ */
