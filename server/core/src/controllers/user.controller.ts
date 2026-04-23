e116f8c6af8166c5a985b1402573c58d0af0b5d8
mayanamsterdam <maayanamsterdam@gmail.com>
2026-04-23 17:22:03 +0300

    add getbyid and update user routes

---
+++ server/core/src/controllers/user.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
+import { UpdateUserDto, UserDto } from "../dtos/user.dto";
+import { userService } from "../services";
+
+export const userRouter = Router();
+
+userRouter.get(
+  "/:id",
+  async (req: Request<{ id: UserDto["id"] }>, res: Response) => {
+    const user = await userService.getById(req.params.id);
+    res.status(StatusCodes.OK).json(user);
+  },
+);
+
+userRouter.put(
+  "/:id",
+  async (
+    req: Request<{ id: UserDto["id"] }, {}, Partial<UpdateUserDto>>,
+    res: Response,
+  ) => {
+    const user = await userService.update(req.params.id, req.body);
+    res.status(StatusCodes.OK).json(user);
+  },
+);
