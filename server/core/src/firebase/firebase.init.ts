f9731118cce1e347d60a1dfabdf8be3f6cb40808
Guy Barry <guybarry5@gmail.com>
2026-04-23 18:18:50 +0300

    remove firebase

--- server/core/src/firebase/firebase.init.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin';
-import { firebaseConfig } from '../config/firebase.config';
-
-if (!admin.apps.length) {
-  admin.initializeApp(firebaseConfig);
-}
-
-export const firebaseApp = admin.app();
