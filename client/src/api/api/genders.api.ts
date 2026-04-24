05839e3b939c6195090bf4927671dd43b312e0c9
karensproject <karenitz2708@gmail.com>
2026-04-24 17:19:56 +0300

    refactor(api): add single axios client factory

--- client/src/api/api/genders.api.ts
+++ client/src/api/api/genders.api.ts
@@ -1,13 +1,9 @@
-import axios from 'axios';
+import apiClient from '../client';
 import type { Gender } from '../../entities';
-
-const client = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/genders`,
-});
 
 export const gendersApi = {
   getAll: async (): Promise<Gender[]> => {
-    const { data } = await client.get<Gender[]>('/');
+    const { data } = await apiClient.get<Gender[]>('/genders');
     return data;
   },
 };
